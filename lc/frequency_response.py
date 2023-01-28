ef4b25bac4ee336c056c60b8fa1b6e516aa6e5c6
John Scott <johnrscott0@gmail.com>
2023-01-28 18:08:01 +0000

    Added a simple loop to try out setting frequency then reading waveform

--- lc/frequency_response.py
+++ lc/frequency_response.py
@@ -3,21 +3,28 @@ from fy6600 import FY6600
 from time import sleep
 import numpy as np
 
-#gen = FY6600()
+gen = FY6600()
+
 osc = DS1054Z()
-
-f = np.geomspace(1e3, 1e4, 11)
-print(f)
-
-#gen.set_frequency(1e3)
-#gen.set_amplitude(1.5)
-
-exit()
-
 osc.reset()
 osc.unlock_autoscale()
+
+freq = np.geomspace(1e3, 1e4, 11)
+print(freq)
+
+gen.set_amplitude(1.0)
+gen.set_frequency(freq[0])
 osc.autoscale()
 
-osc.average_vpp(1)
-osc.average_phase_difference(1,2)
+for f in freq:
+    gen.set_frequency(f)
+    sleep(1)
+    osc.reset_statistic_data()
+    sleep(3)
+    osc.average_vpp(1)
+    osc.average_phase_difference(1,2)
+    
+    
+exit()
+
 
