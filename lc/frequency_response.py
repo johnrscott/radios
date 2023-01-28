cf3c0f227ab927207ef2af3dd7efad211b29097b
John Scott <johnrscott0@gmail.com>
2023-01-28 17:45:07 +0000

    Oscilloscope timing out now

--- lc/frequency_response.py
+++ lc/frequency_response.py
@@ -1,14 +1,19 @@
 from ds1054z import DS1054Z
 from fy6600 import FY6600
 from time import sleep
+import numpy as np
 
-gen = FY6600()
-gen.set_frequency(1e3)
-gen.set_amplitude(1.5)
+#gen = FY6600()
+osc = DS1054Z()
+
+f = np.geomspace(1e3, 1e4, 11)
+print(f)
+
+#gen.set_frequency(1e3)
+#gen.set_amplitude(1.5)
 
 exit()
 
-osc = DS1054Z()
 osc.reset()
 osc.unlock_autoscale()
 osc.autoscale()
