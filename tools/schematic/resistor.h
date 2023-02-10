e964618c24caae68ca709d8807d088ca2784a3fe
John Scott <johnrscott0@gmail.com>
2023-02-10 19:18:54 +0000

    Added resistor shape

--- tools/schematic/resistor.h
+++ tools/schematic/resistor.h
@@ -11,16 +11,25 @@ public:
     Resistor();
     QRectF boundingRect() const override
     {
-        qreal penWidth = 1;
-        return QRectF(-10 - penWidth / 2, -10 - penWidth / 2,
-                      20 + penWidth, 20 + penWidth);
+        qreal bound_width{width_ + 2*lead_length_ + pen_width_};
+        qreal bound_height{height_ + pen_width_};
+        return QRectF(bound_width/2, -bound_height/2,
+                      bound_width, bound_height);
     }
 
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
                QWidget *widget) override
     {
-        painter->drawRoundedRect(-10, -10, 20, 20, 5, 5);
+        painter->drawRect(-width_/2, -height_/2, width_, height_);
+        painter->drawLine(-width_/2, 0, -width_/2 - lead_length_, 0);
+        painter->drawLine(width_/2, 0, width_/2 + lead_length_, 0);
     }
+private:
+    qreal pen_width_{1};
+    qreal width_{50};
+    qreal height_{15};
+    qreal lead_length_{20};
+
 };
 
 #endif // RESISTOR_H
